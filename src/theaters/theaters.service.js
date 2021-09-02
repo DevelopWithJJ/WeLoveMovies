4f61679e03a3d98a29869330487c8895182a5e64
Jevon <jevonjackson96@gmail.com>
2021-09-02 10:19:14 -0500

    Fixed reduceMovies to properly render APIs

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -8,22 +8,14 @@ const reduceMovies = reduceProperties("theater_id", {
   rating: ["movies", null, "rating"],
   description: ["movies", null, "description"],
   image_url: ["movies", null, "image_url"],
-  created_at: ["movies", null, "created_at"],
-  updated_at: ["movies", null, "updated_at"],
   is_showing: ["movies", null, "is_showing"],
-  theater_id: ["movies", null, "theater_id"],
 });
 
 function list(movie_id) {
-  let output = knex("theaters as t")
-    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-    .join("movies as m", "m.movie_id", "mt.movie_id")
-    .select("t.*", "m.*");
-  if (movie_id) {
-    output = output.where("m.movie_id", movie_id);
-  }
-
-  return output.then(reduceMovies);
+  return knex("theaters as t")
+    .join("movies_theaters", "t.theater_id", "movies_theaters.theater_id")
+    .join("movies as m", "m.movie_id", "movies_theaters.movie_id")
+    .then(reduceMovies);
 }
 
 module.exports = {
